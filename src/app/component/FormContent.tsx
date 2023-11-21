02e81f78b6bb6ffa6299d4dc38fcea3b0fa6ca91
Muhammad Gibran Af Kara <gibranafkara12@gmail.com>
2023-11-22 01:37:38 +0700

    some code added

--- src/app/component/FormContent.tsx
+++ src/app/component/FormContent.tsx
@@ -2,43 +2,50 @@ import { useState } from "react";
 
 function FormContent() {
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(false);
 
 	function handlePrev() {
 		if (step > 1) {
-			setStep(step - 1);
+			setStep((step) => step - 1);
 			console.log(step);
 		}
 	}
 
 	function handleNext() {
 		if (step < 3) {
-			setStep(step + 1);
+			setStep((step) => step + 1);
 			console.log(step);
 		}
 	}
 
 	return (
 		<>
-			<div className="steps">
-				<div className="numbers">
-					<div className={step >= 1 ? "active" : ""}>1</div>
-					<div className={step >= 2 ? "active" : ""}>2</div>
-					<div className={step >= 3 ? "active" : ""}>3</div>
+			<button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+				$times
+			</button>
+
+			{isOpen && (
+				<div className="steps">
+					<div className="numbers">
+						<div className={step >= 1 ? "active" : ""}>1</div>
+						<div className={step >= 2 ? "active" : ""}>2</div>
+						<div className={step >= 3 ? "active" : ""}>3</div>
+					</div>
+					<p className="message">Muhammad Gibran</p>
+					<div className="buttons">
+						<button
+							style={{ backgroundColor: "#526D82", color: "#fff" }}
+							onClick={handlePrev}>
+							Prev
+						</button>
+						<button
+							style={{ backgroundColor: "#526D82", color: "#fff" }}
+							onClick={handleNext}>
+							Next
+						</button>
+					</div>
 				</div>
-				<p className="message">Muhammad Gibran</p>
-				<div className="buttons">
-					<button
-						style={{ backgroundColor: "#526D82", color: "#fff" }}
-						onClick={handlePrev}>
-						Prev
-					</button>
-					<button
-						style={{ backgroundColor: "#526D82", color: "#fff" }}
-						onClick={handleNext}>
-						Next
-					</button>
-				</div>
-			</div>
+			)}
 		</>
 	);
 }
